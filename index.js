af238b971519b4c0833a2b42ae3e1157743bdaf6
Romando Purba <romando.purba@yahoo.com>
2022-10-31 20:01:24 +0700

    Add some feature

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 const port = process.env.APP_PORT || 3000;
 
 // Import routes here
-const exampleRoute = require("./routes/examples");
+const route = require('./routes/router')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Use your route here
-app.use("/api/v1/example", exampleRoute);
+app.use("/api/v1/books", route);
 
 app.get('/', (req, res) => {
   res.json('Okay')
