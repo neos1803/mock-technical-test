fc581c1a55fd76905c7f7f6835c4e71046acc4b5
Manges23 <tempestdragon987@gmail.com>
2023-09-27 19:55:01 +0700

    add error response, eslint & SQL challenge question

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # mock-technical-test
+
+# SQL Challenge 1
+
+Customers Table
+![image](https://user-images.githubusercontent.com/38176579/198935485-bf96c15e-45d8-43d7-ba44-b4fd39253212.png)
+
+Orders Table
+![image](https://user-images.githubusercontent.com/38176579/198935570-91aad51c-a34d-4aaa-bd7f-b6ba58628cc4.png)
+
+The challenge is to find customers who don't make any order or don't have any order. Create a query that will return a result like the image below
+![image](https://user-images.githubusercontent.com/38176579/198941947-d002afc5-3176-46fe-8d41-7f9dd59249ae.png)
+
+## SQL Challenge 1 Answers
+```sql
+SELECT `customers`.`name`
+FROM `customers`
+WHERE NOT EXISTS(
+    SELECT 1 FROM `orders`
+    WHERE `orders`.`customer_id` = `orders`.`id`
+);
+```
+
+# SQL Challenge 2
+Table mahasiswa
+
+![image](https://user-images.githubusercontent.com/38176579/198942375-1bf45f81-7ec6-4eb4-8078-7c9fcc4d0b3a.png)
+
+Challenge:
+Buatlah query untuk menemukan NIM yang memiliki duplikasi kode_matakuliah seperti contoh di bawah
+
+![image](https://user-images.githubusercontent.com/38176579/198942457-dab82929-ac5e-4274-8a2c-27eca2ff1cee.png)
+
+## SQL Challenge 2 Answers
+
+```sql
+SELECT *, COUNT(*) AS Jumlah_duplikasi
+FROM mahasiswa
+GROUP BY nim, kode_matakuliah
+HAVING Jumlah_duplikasi > 1;
+```
--- controllers/BookController.js
+++ controllers/BookController.js
@@ -1,71 +1,136 @@
 const books = require('../models/data');
 
+
 class BooksController {
-
-    static async getAllBooks(req, res) {
+  static async getAllBooks(req, res) {
+    try {
       return res.status(200).json({
-        message: "Success",
-        data: books
-      })
+        message: 'Success',
+        data: books,
+      });
+    } catch (error) {
+      const errResponse = {
+        status_response: false,
+        message: error.message,
+        errors: error,
+        data: null,
+      };
+      res.status(500).send(errResponse);
     }
-
-    static async getBookById(req, res) {
-        const bookId = req.params.id
-        const book = books.find(({id}) => id == bookId)
-        return res.status(200).json({
-            message: "Success",
-            data: book
-        })
-    }
-
-    static async getBookByType(req, res) {
-        const {jenis} = req.params
-        const book = books.filter(({type}) => type.toLowerCase() == jenis.toLowerCase())
-        return res.status(200).json({
-            message: "Success",
-            data: book
-        })
-    }
-
-    static async addBook(req, res) {
-        const {name, type} = req.body
-        const book = {
-            id: books.length + 1,
-            name,
-            type,
-        }
-        books.push(book)
-        return res.status(200).json({
-            message: "Success",
-            data: books
-        })
-    }
-
-    static async editBookById(req, res) {
-        const bookId = req.params.id
-        const theBook = books.findIndex(({id}) => id == bookId)
-        const {name, type} = req.body
-        books[theBook] = {
-            ...books[theBook],
-            name,
-            type
-        }
-        return res.status(200).json({
-            message: "Success",
-            data: books
-        })
-    }
-
-    static async deleteBookById(req, res) {
-        const bookId = req.params.id
-        const book = books.findIndex(({id}) => id == bookId)
-        books.splice(book, 1)
-        return res.status(200).json({
-            message: "Success",
-            data: books
-        })
-    }
-  
   }
-  
-  module.exports = BooksController;
+
+  static async getBookById(req, res) {
+    try {
+      const bookId = req.params.id;
+      const book = books.find(({id}) => id == bookId);
+      if (!book) {
+        return res.status(200).json({
+          message: 'Book Not Found!',
+          data: book,
+        });
+      }
+      return res.status(200).json({
+        message: 'Success',
+        data: book,
+      });
+    } catch (error) {
+      const errResponse = {
+        status_response: false,
+        message: error.message,
+        errors: error,
+        data: null,
+      };
+      res.status(500).send(errResponse);
+    }
+  }
+
+  static async getBookByType(req, res) {
+    try {
+      const {jenis} = req.params;
+      const book = books.filter(({type}) => type.toLowerCase() == jenis.toLowerCase());
+      return res.status(200).json({
+        message: 'Success',
+        data: book,
+      });
+    } catch (error) {
+      const errResponse = {
+        status_response: false,
+        message: error.message,
+        errors: error,
+        data: null,
+      };
+      res.status(500).send(errResponse);
+    }
+  }
+
+  static async addBook(req, res) {
+    try {
+      const {name, type} = req.body;
+      const book = {
+        id: books.length + 1,
+        name,
+        type,
+      };
+      books.push(book);
+      return res.status(200).json({
+        message: 'Success',
+        data: books,
+      });
+    } catch (error) {
+      const errResponse = {
+        status_response: false,
+        message: error.message,
+        errors: error,
+        data: null,
+      };
+      res.status(500).send(errResponse);
+    }
+  }
+
+  static async editBookById(req, res) {
+    try {
+      const bookId = req.params.id;
+      const theBook = books.findIndex(({id}) => id == bookId);
+      const {name, type} = req.body;
+      books[theBook] = {
+        ...books[theBook],
+        name,
+        type,
+      };
+      return res.status(200).json({
+        message: 'Success',
+        data: books,
+      });
+    } catch (error) {
+      const errResponse = {
+        status_response: false,
+        message: error.message,
+        errors: error,
+        data: null,
+      };
+      res.status(500).send(errResponse);
+    }
+  }
+
+  static async deleteBookById(req, res) {
+    try {
+      const bookId = req.params.id;
+      const book = books.findIndex(({id}) => id == bookId);
+      books.splice(book, 1);
+      return res.status(200).json({
+        message: 'Success',
+        data: books,
+      });
+    } catch (error) {
+      const errResponse = {
+        status_response: false,
+        message: error.message,
+        errors: error,
+        data: null,
+      };
+      res.status(500).send(errResponse);
+    }
+  }
+}
+
+module.exports = BooksController;
